7298287a6dcc0ab54a751fbc79bc5808dd8bdbb1
wwwins <jacky.huang@isobar.com>
2021-03-24 10:33:48 +0800

    Update doc.xpath

--- Finance/stock_2412.1m.py
+++ Finance/stock_2412.1m.py
@@ -12,9 +12,12 @@ import time
 from lxml import html
 
 # Setting price to buy a stock
-BUY_PRICE = 95.0
+BUY_PRICE = 100.0
 STOCK = "2412"
 color = "steelblue"
+
+HEADER = {'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 8_0_2 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4'}
+URL = 'https://tw.stock.yahoo.com/q/q?s='+STOCK+'&_ts='
 
 if (int(time.strftime("%H")) > 17):
 	print ('🈚️')
@@ -24,12 +27,14 @@ if (int(time.strftime("%H")) < 9):
 	print ('🈚️')
 	exit()
 
-ts = int(time.time());
-r = requests.get("https://tw.m.yahoo.com/w/twstock/index_single.php?_ts="+str(ts)+"&_httpHost=tw.m.yahoo.com&_intl=tw&_lang=zh-hant-tw&stock="+STOCK)
+ts = str(int(time.time()));
+r = requests.get(URL+ts, headers=HEADER)
 doc = html.fromstring(r.content)
-content = doc.cssselect("tr.odd")
-price = content[0][2].text
+content = doc.xpath('//div[@id="main-0-QuoteHeader-Proxy"]/span')
+if not content:
+	print ('🈚️')
+	exit()
+price = content[0].text
 if (float(price) < BUY_PRICE):
 	color = "red"
-
 print ('Chunghwa:'+price+'| color='+color)
